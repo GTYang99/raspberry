be448b74e83eac4193c453e27146b746ab0f23b2
GT.Yang <gaga0099@gmail.com>
2022-10-28 00:07:20 +0800

    新增LightButton的按鈕

--- 專案1_LedControl/main.py
+++ 專案1_LedControl/main.py
@@ -11,12 +11,31 @@ firebase_admin.initialize_app(cred,{
 })
 ref = db.reference('ledcontrol')
 
+class LightButton(tk.Button):
+    def __init__(self,parent,**kwargs):
+        super().__init__(parent,**kwargs)
+        #建立圖片
+        ##建立close的圖片
+        close_image = Image.open('light_close.png')
+        self.close_photo = ImageTk.PhotoImage(close_image)
+        ##建立open的圖片
+        open_image = Image.open('light_open.png')
+        self.open_photo = ImageTk.PhotoImage(open_image)
+
+    def open(self):
+        self.config(image=self.open_photo)
+
+    def close(self):
+        self.config(image=self.close_photo);   
+
+
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         #建立title
         self.title("LED Controller")
         #建立按鈕
+        self.btn = LightButton(self,padx=50,pady=30,font=('arial',18),command=self.userClick)
         # btn = tk.Button(self,text="開關",padx=50,pady=30,font=('arial',18),command=self.userClick)
         # btn.pack(padx=50,pady=30)
         # self.btn = tk.Button(self,text="開關",padx=50,pady=30,font=('arial',18),command=self.userClick)
@@ -29,9 +48,9 @@ class Window(tk.Tk):
         self.btn.pack(padx=50,pady=30)
         currentState = ref.get()['led']
         if currentState:
-            self.btn.config(image=self.close_photo)
+            self.btn.close()
         else:
-            self.btn.config(image=self.open_photo)
+            self.btn.open()
 
     def userClick(self):
         # print("user click")
@@ -39,10 +58,9 @@ class Window(tk.Tk):
         currentState = ref.get()['led']
         ref.update({'led':not currentState})
         if currentState:
-            self.btn.config(image=self.open_photo)
+            self.btn.open()
         else:
-            self.btn.config(image=self.close_photo)
-
+            self.btn.close()
 def main():
     pass
 

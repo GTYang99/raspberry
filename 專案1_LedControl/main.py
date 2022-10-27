4b3bebbdf39395d35d36c7e318abb4b8c0c9e435
GT.Yang <gaga0099@gmail.com>
2022-10-28 00:09:01 +0800

    修改成上課版本

--- 專案1_LedControl/main.py
+++ 專案1_LedControl/main.py
@@ -1,8 +1,8 @@
 import tkinter as tk
+from PIL import Image,ImageTk
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-from PIL import Image,ImageTk
 
 cred = credentials.Certificate("private/raspberry1-45ee2-firebase-adminsdk-5h0yc-149b6394cf.json")
 firebase_admin.initialize_app(cred,{
@@ -10,6 +10,7 @@ firebase_admin.initialize_app(cred,{
     'databaseURL':'https://raspberry1-45ee2-default-rtdb.firebaseio.com/'
 })
 ref = db.reference('ledcontrol')
+# print(ref)
 
 class LightButton(tk.Button):
     def __init__(self,parent,**kwargs):
@@ -21,48 +22,59 @@ class LightButton(tk.Button):
         ##建立open的圖片
         open_image = Image.open('light_open.png')
         self.open_photo = ImageTk.PhotoImage(open_image)
+        self.config(borderwidth=0)
+        self.config(font=('arial',18))
 
     def open(self):
         self.config(image=self.open_photo)
+        self.config(text="關")
 
     def close(self):
-        self.config(image=self.close_photo);   
+        self.config(image=self.close_photo)
+        self.config(text="開")
 
-
-class Window(tk.Tk):
+class window(tk.Tk):
     def __init__(self):
         super().__init__()
-        #建立title
-        self.title("LED Controller")
-        #建立按鈕
-        self.btn = LightButton(self,padx=50,pady=30,font=('arial',18),command=self.userClick)
-        # btn = tk.Button(self,text="開關",padx=50,pady=30,font=('arial',18),command=self.userClick)
-        # btn.pack(padx=50,pady=30)
-        # self.btn = tk.Button(self,text="開關",padx=50,pady=30,font=('arial',18),command=self.userClick)
+        # 建立title
+        self.title('LED Controller')
         # 建立圖片
-        close_image = Image.open('light_close.png')
-        self.close_photo = ImageTk.PhotoImage(close_image)
-        open_image = Image.open('light_open.png')
-        self.open_photo = ImageTk.PhotoImage(open_image)
-        self.btn = tk.Button(self,image=self.close_photo,padx=50,pady=30,font=('arial',18),command=self.userClick)
+        # close_image = Image.open('light_close.png')
+        # self.close_photo = ImageTk.PhotoImage(close_image)
+        # self.btn = tk.Button(self,image=self.close_photo,padx=50,pady=30,font=('arial',18),command=self.userClick)
+
+        # open_image = Image.open('light_open.png')
+        # self.open_photo = ImageTk.PhotoImage(open_image)
+        # self.btn = tk.Button(self,image=self.open_photo,padx=50,pady=30,font=('arial',18),command=self.userClick)
+
+        # 建立按鈕
+        # self.btn = tk.Button(self,text='開關',padx=50,pady=30,font=('arial',18),command=self.userClick)
+        self.btn = LightButton(self,padx=50,pady=30,command=self.userClick)
         self.btn.pack(padx=50,pady=30)
+        # 建立讀取數據庫的資料，改變開關字樣
         currentState = ref.get()['led']
         if currentState:
-            self.btn.close()
-        else:
             self.btn.open()
+            # self.btn.config(image=self.close_photo)
+        else:
+            self.btn.close()
+            # self.btn.config(image=self.open_photo)
 
     def userClick(self):
-        # print("user click")
+        # 收集led的資料
         # print(ref.get())
         currentState = ref.get()['led']
         ref.update({'led':not currentState})
         if currentState:
-            self.btn.open()
-        else:
             self.btn.close()
+            # self.btn.config(image=self.close_photo)
+        else:
+            self.btn.open()
+            # self.btn.config(image=self.open_photo)
+
 def main():
-    pass
+    windows = window()
+    windows.mainloop()
 
 if __name__ == "__main__":
     main()

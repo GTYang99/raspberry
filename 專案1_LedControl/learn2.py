53266a2455aa80bbe3d26fc928ffce08f87d197e
GT.Yang <gaga0099@gmail.com>
2022-10-28 14:30:26 +0800

    Canvas長方形

--- 專案1_LedControl/learn2.py
+++ 專案1_LedControl/learn2.py
@@ -7,12 +7,12 @@ class Window(tk.Tk):
         # 建立一個tk功能中的Canvas
         canvas1 = tk.Canvas(self,width=400,height=250,background='#BF6766')
         # 線條是利用座標格式去畫
-        # 創建一條橫實線
-        # canvas1.create_line(15,30,200,30)
-        # 創建一條直虛線
-        # canvas1.create_line(300,35,300,200,dash=(4,2))
-        # 創建一個三角形
-        # canvas1.create_line(55, 85, 155,85,105,180,55, 85,fill="#FFBA84")
+        # 創建長方形方法
+        canvas1.create_rectangle(30,10,120,80,outline='#000')
+        canvas1.create_rectangle(150, 10, 240, 80, outline="#000", fill="#f50")
+        canvas1.create_rectangle(270, 10, 370, 80, outline="#000", fill="#05f")
+        # 創建曲線方法
+        canvas1.create_arc(1,1,10,10)
         # tk的YES就是true
         canvas1.pack(fill=tk.BOTH,expand=tk.YES)
 

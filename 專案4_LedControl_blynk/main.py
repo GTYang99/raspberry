0cd64e2902af072516cf52d5c5b8a6830a8ecd98
GT.Yang <gaga0099@gmail.com>
2022-11-15 14:18:12 +0800

    建置程式基礎

--- 專案4_LedControl_blynk/main.py
+++ 專案4_LedControl_blynk/main.py
@@ -1,20 +1,28 @@
 import tkinter as tk
 # 引用這項功能的Class被移到tools.py去，因此會變暗色，表示main.py內容沒有使用到
-# from PIL import Image,ImageTk
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+from PIL import Image,ImageTk
 import RPi.GPIO as GPIO
 # 確認載入的模塊是使用物件導向或者是function導向寫的
 from tools import LightButton
 
-cred = credentials.Certificate("private/raspberry1-45ee2-firebase-adminsdk-5h0yc-acdceb5727.json")
-firebase_admin.initialize_app(cred,{
-    # 這邊databasURL的URL要大寫
-    'databaseURL':'https://raspberry1-45ee2-default-rtdb.firebaseio.com/'
-})
-ref = db.reference('ledcontrol')
-# print(ref)
+class LightButton(tk.Button):
+    def __init__(self,parent,**kwargs):
+        super().__init__(parent,**kwargs)
+        #建立圖片
+        ##建立close的圖片
+        close_image = Image.open('light_close.png')
+        self.close_photo = ImageTk.PhotoImage(close_image)
+        ##建立open的圖片
+        open_image = Image.open('light_open.png')
+        self.open_photo = ImageTk.PhotoImage(open_image)
+        self.config(borderwidth=0)
+        self.config(state="disabled")
+        
+    def open(self):
+        self.config(image=self.open_photo)
+        
+    def close(self):
+        self.config(image=self.close_photo);
 
 
 class window(tk.Tk):
@@ -33,9 +41,11 @@ class window(tk.Tk):
 
         # 建立按鈕
         # self.btn = tk.Button(self,text='開關',padx=50,pady=30,font=('arial',18),command=self.userClick)
-        self.btn = LightButton(self,padx=50,pady=30,command=self.userClick)
+        self.btn = LightButton(self,padx=50,pady=30)
         self.btn.pack(padx=50,pady=30)
+        self.btn.open()
         # 建立讀取數據庫的資料，改變開關字樣
+        '''
         currentState = ref.get()['led']
         if currentState:
             # GPIO BVM25設定為開啟時，給出高的電壓
@@ -49,7 +59,7 @@ class window(tk.Tk):
             GPIO.output(25,GPIO.LOW)
             # self.btn.config(image=self.open_photo)
             self.btn.light_state = False
-
+        
     def userClick(self):
         # 收集led的資料
         # print(ref.get())
@@ -65,7 +75,7 @@ class window(tk.Tk):
             GPIO.output(25,GPIO.HIGH)
             # self.btn.config(image=self.open_photo)
             self.btn.light_state = True
-
+        '''
 def main():
     # GPIO的基礎設定模式，
     GPIO.setmode(GPIO.BCM)
